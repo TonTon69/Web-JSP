92b954c6e8a20f7d854fca881a60f0d233ff3356
TonTon69 <hoanghuynh0609@gmail.com>
2021-06-03 16:12:40 +0700

    change db config in application file

--- src/java/connect/DBConnect.java
+++ src/java/connect/DBConnect.java
@@ -8,7 +8,7 @@ public class DBConnect {
     public static Connection getConnecttion() {
         Connection cons = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.postgresql.jdbc.Driver");
             cons = (Connection) DriverManager.getConnection(
                     "jdbc:postgresql://ec2-50-17-255-120.compute-1.amazonaws.com:5432/d6cobgbmuv17je", "fuihfoupedayet", "2454efa05cc2de4103c3b718ca1b1185d39f806d18e880a8446cfa979ff18c54");
         } catch (Exception e) {
